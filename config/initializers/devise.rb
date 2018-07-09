8b20f63dca2fe5a779ca27540e0bac11aea9be26
Akash Manhas <akash.blogvault.net>
2018-07-09 15:25:49 +0530

    Custom Devise Login + Signup System

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,8 @@
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
-  # The secret key used by Devise. Devise uses this key to generate
+Devise.setup do |config|
+# The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
@@ -228,7 +229,7 @@
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+   config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
